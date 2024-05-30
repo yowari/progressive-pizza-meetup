d2ede7e6b81ea1f13fb3252d2c77fe635995de94
yowari <yowari@users.noreply.github.com>
2024-05-30 20:24:54 +0200

    04-action

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +7,55 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const size = formData.get("size");
+  const toppings = formData.getAll("toppings");
+
+  console.log(`Ordering a ${size} pizza with ${toppings.join(", ")}`);
+
+  return new Response(null, {
+    status: 302,
+    headers: { Location: "/confirmation" },
+  });
+}
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <h2>Remixez votre pizza</h2>
+
+      <form method="POST" action="?index">
+        <fieldset>
+          <legend>Selectionnez la taille</legend>
+
+          <label>
+            <input type="radio" name="size" value="small" />
+            Small
+          </label>
+
+          <label>
+            <input type="radio" name="size" value="medium" />
+            medium
+          </label>
+
+          <label>
+            <input type="radio" name="size" value="large" />
+            Large
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Choisissez votre garniture</legend>
+
+          <label>
+            <input type="checkbox" name="toppings" value="pepperoni" />
+            Pepperoni
+          </label>
+        </fieldset>
+
+        <button type="submit">Commander</button>
+      </form>
+    </main>
   );
 }
